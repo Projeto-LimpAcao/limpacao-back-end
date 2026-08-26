598a5b88f2b3ebc1ffbdff194af36f2d5fbdb31e
LucasCinigalia <cinigalialucas@gmail.com>
2026-08-26 20:28:36 -0300

    Estrutura projeto back-end

---
+++ src/main/java/com/limpacao/LimpacaoApplication.java
@@ -0,0 +1,13 @@
+package com.limpacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimpacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimpacaoApplication.class, args);
+	}
+
+}
